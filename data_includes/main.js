965465bfeef4f22874e89cc3243e78e11813a1a1
xlwwww <517431424@qq.com>
2020-08-23 16:59:24 +0800

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -31,7 +31,7 @@ newTrial( "welcome" ,
     ,
     newImage("4","qsx.png")
          .size(100,100)
-    , 
+    ,
     newCanvas(200,200)
          .add(   0 , 0 , getImage("1") )
          .add(   100 , 0 , getImage("2") )
@@ -51,7 +51,7 @@ newTrial( "welcome" ,
         .set( getTextInput("inputID") )
 )
 .log( "ID" , getVar("ID") )
-Template( variable => 
+Template( variable =>
     newTrial( "experiment" ,
     newAudio('bgm',"drumloop_65.wav")
         .play()
@@ -113,6 +113,66 @@ Template( variable =>
   .log( "Item"   , variable.Item   )
   .log( "Group"  , variable.Group  )
 )
+var startx, starty;
+
+//获得角度
+function getAngle(angx, angy) {
+  return Math.atan2(angy, angx) * 180 / Math.PI;
+};
+
+//根据起点终点返回方向 1向上滑动 2向下滑动 3向左滑动 4向右滑动 0点击事件
+function getDirection(startx, starty, endx, endy) {
+var angx = endx - startx;
+var angy = endy - starty;
+var result = 0;
+
+//如果滑动距离太短
+if (Math.abs(angx) < 2 && Math.abs(angy) < 2) {
+    return result;
+}
+
+var angle = getAngle(angx, angy);
+if (angle >= -135 && angle <= -45) {
+    result = 1;
+} else if (angle > 45 && angle < 135) {
+    result = 2;
+} else if ((angle >= 135 && angle <= 180) || (angle >= -180 && angle < -135)) {
+    result = 3;
+} else if (angle >= -45 && angle <= 45) {
+    result = 4;
+}
+return result;
+}
+document.addEventListener("touchestart", function(e){
+  startx = e.touches[0].pageX;
+  starty = e.touches[0].pageY;
+}, false);
+document.addEventListener("touchend", function(e) {
+  var endx, endy;
+  endx = e.changedTouches[0].pageX;
+  endy = e.changedTouches[0].pageY;
+  var direction = getDirection(startx, starty, endx, endy);
+  switch (direction) {
+    case 0:
+        alert("点击！");
+        break;
+    case 1:
+        alert("向上！");
+        break;
+    case 2:
+        alert("向下！");
+        break;
+    case 3:
+        alert("向左！");
+        break;
+    case 4:
+        alert("向右！");
+        break;
+    default:
+        alert("点击！");
+        break;
+    }
+  }, false);
 SendResults( "send" )
 newTrial( "final" ,
     newText("<p>Thank you for your participation!</p>")
